46ff9e40a838fb0cd30900dab1cf2faad1bfbd7c
Ashish Jha <ajha5645@gmail.com>
2024-07-13 22:49:57 +0530

    hibernate 1st commit

---
+++ src/main/java/org/ashish/hibernatelearning/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.ashish.hibernatelearning;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/org/ashish/model/Student.java
@@ -0,0 +1,59 @@
+package org.ashish.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "studentTable") //not required if table name is same as class name
+public class Student {
+
+    @Id //primary key
+    @Column(name = "studentId") //not required if column name is same as field name
+    private int studentId;
+
+    private String studentName;
+    private String studentAddress;
+    private String studentEmail;
+
+//    make a zero argument constructor for the hibernate to work
+
+
+    public Student() {
+        System.out.println("Student object created for hibernate");
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
