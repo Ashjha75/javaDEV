5f96934b0aef9d986debca1ccaf4f699a0e971a4
Ashish Jha <ajha5645@gmail.com>
2024-08-04 13:37:50 +0530

    added base commit for springBoot Jdbc

---
+++ src/test/java/com/ashish/Spring/JDBCEX/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.Spring.JDBCEX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
